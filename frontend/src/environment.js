a9bff703cd53ce43273b7f34902bbf50b3fe48f7
Vishrut Bharti <vishrut85085@gmail.com>
2026-08-16 11:24:56 +0530

    Change server URL for production environment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 let IS_PROD = true;
 
 const server = IS_PROD ?
-    "https://video-conferencing-app-fb11.onrender.com" :
+    "https://video-conferencing-app-6o6a.onrender.com" :
     "http://localhost:8001";
 
 export default server;
